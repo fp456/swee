a96e1524400947406c9e9659b827b6f5bdb9e3c8
Tatenda Chiduwa <tatenda.f.chiduwa@ou.edu>
2022-07-08 08:04:13 +0000

    Tatenda

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sqlite3
-import pandas as pandas
+
 
 conn = sqlite3.connect('intern_roles.db')
 
@@ -13,8 +13,6 @@ conn.execute('''CREATE TABLE saved_jobs
               LOCATION TEXT,
               LINK TEXT );''')
 
-
-
 def save_job(job):
       title=job.get("title")
       company=job.get("company")
@@ -23,11 +21,10 @@ def save_job(job):
       link=job.get("link")
       conn=sqlite3.connect("intern_roles.db")
       conn.execute(f"insert into saved_jobs (TITLE,COMPANY,DESCRIPTION,LOCATION,LINK) VALUES ('{title}','{company}','{description}','{location}','{link}')")
+      conn.commit()
  
 for row in conn.execute("SELECT* FROM saved_jobs ORDER BY title"):
       print(row)
-
-
 
 def delete_job(id):
     """
@@ -42,19 +39,12 @@ def delete_job(id):
     cur.execute(sql, (id,))
     conn.commit()
     cur.execute("SELECT * FROM saved_jobs")
-    print(cur.fetchall())
-
-save_job({"title":"Engineer","company":"Amazon","location":"San Fransisco","description":"Get the job","link":"https://something"})
+    conn.commit()
 
 cur=conn.cursor()
 cur.execute("SELECT * FROM saved_jobs")
 print(cur.fetchall())
   
-delete_job(1)
-    
-    
-
-
 conn.commit()
 
 conn.close()
