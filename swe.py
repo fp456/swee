a45369b12fa869e88f307970cb8d48aa431102ae
Isaiah Swain <iswain@emory.edu>
2022-07-08 05:39:38 +0000

    fixed style

--- swe.py
+++ swe.py
@@ -12,31 +12,30 @@ class Backend:
         ADZUNA_ID, ADZUNA_KEY = self.get_token()
         self.executer(country_num, location, ADZUNA_ID, ADZUNA_KEY)
 
-
     def executer(self, country_num, location, id, key):
         url, params = self.prepare_request(id, key, country_num, location)
         self.results = self.get_results(url, params)
 
-
     # Returns next job from the returned array
+
     def next_job(self):
         self.current_job += 1
         if self.current_job >= len(self.results):
             return None
         return self.results[self.current_job - 1]
 
-
     # Getting the Adzuna id, key
+
     def get_token(self):
         return os.environ.get('ADZUNA_ID'), os.environ.get('ADZUNA_KEY')
 
-
     # Returning the ISO3 code of a given country
+
     def get_country_key(self, country_num):
         return countries_codes.countries_dict[country_num]
 
-
     # Prepering the url and parameters
+
     def prepare_request(self, id, key, country_num, location):
         params = {}
         country_code = self.get_country_key(country_num)
@@ -55,8 +54,8 @@ class Backend:
         params = urllib.parse.urlencode(params, quote_via=urllib.parse.quote)
         return url, params
 
-
     # Get a request
+
     def get_results(self, url, params):
         data = requests.get(url, params=params).json()
         return data['results']
